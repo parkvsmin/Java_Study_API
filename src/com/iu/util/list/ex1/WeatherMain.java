187061150da6c53c422c41f34af22c1591cfef06
parkvsmin <sdfjkl658@naver.com>
2022-07-20 21:22:19 +0900

    0720

--- src/com/iu/util/list/ex1/WeatherMain.java
+++ src/com/iu/util/list/ex1/WeatherMain.java
@@ -14,23 +14,21 @@ public class WeatherMain {
 		
 		WeatherView wv = new WeatherView();
 		WeatherService ws = new WeatherService();
-		WeatherController st = new WeatherController();
-		
-		CityDTO cityDTO = new CityDTO();
-		cityDTO.setName("NY");
-		cityDTO.setGion(31.2);
-		cityDTO.setHum(20);
-		cityDTO.setStatus("비");
-		
-		CityDTO cityDTO2 = new CityDTO();
-		cityDTO2.setName("LA");
-		cityDTO2.setGion(18);
-		cityDTO2.setHum(90);
-		cityDTO2.setStatus("눈");
-		
-		ArrayList<CityDTO> ar = new ArrayList<>();
-		ar.add(cityDTO);
-		ar.add(cityDTO2);
+//		CityDTO cityDTO = new CityDTO();
+//		cityDTO.setName("NY");
+//		cityDTO.setGion(31.2);
+//		cityDTO.setHum(20);
+//		cityDTO.setStatus("비");
+//		
+//		CityDTO cityDTO2 = new CityDTO();
+//		cityDTO2.setName("LA");
+//		cityDTO2.setGion(17);
+//		cityDTO2.setHum(90);
+//		cityDTO2.setStatus("눈");
+//		
+//		ArrayList<CityDTO> ar = new ArrayList<>();
+//		ar.add(cityDTO);
+//		ar.add(cityDTO2);
 		
 		ArrayList<CityDTO> ar = new ArrayList<>();
 		ws.init(ar);
@@ -40,6 +38,8 @@ public class WeatherMain {
 		wv.view(ar);
 		
 		
+		
+
 	}
 
 }
--- src/com/iu/util/list/ex1/WeatherService.java
+++ src/com/iu/util/list/ex1/WeatherService.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-import com.iu.util.list.ex1.CityDTO;
-
 public class WeatherService {
 	
 //	init메서드
@@ -21,47 +19,22 @@ public class WeatherService {
 //	도시명을 입력받아서 리스트에서 검색
 	
 	private StringBuffer sb;
+	private Scanner sc;
 	
-	public StringBuffer getSb() {
-		return sb;
-	}
-
-	public void setSb(StringBuffer sb) {
-		this.sb = sb;
-	}
-	Scanner sc;
 	public WeatherService() {
 		sc = new Scanner(System.in);
-	
 		sb = new StringBuffer();
 		sb.append("Seoul,30.2,70,맑음,");
 		sb.append("Daegu-38.9-90-흐림-");
-		sb.append("Jeju,12.1,10,눈");
+		sb.append("Jeju,12.1,10,눈,");
 		sb.append("Busan-0.5,85,태풍");
 	}
 	
 	public void init(ArrayList<CityDTO> ar) {
+		String data = sb.toString();
+		data=data.replace("-", ",");
 		
-	}
-	
-	public void add(ArrayList<CityDTO> ar) {
-		CityDTO cityDTO = new CityDTO();
-		cityDTO.setName(sc.next());
-		System.out.println("도시명 입력");
-		cityDTO.setGion(sc.nextDouble());
-		System.out.println("기온 입력");
-		cityDTO.setHum(sc.nextInt());
-		System.out.println("습도 입력");
-		cityDTO.setStatus(sc.next());
-		System.out.println("날씨 입력");
-		ar.add(cityDTO);
-	}
-	
-	
-	public void init(ArrayList<CityDTO> ar) {
-		String data =sb.toString();
-		data =data.replace("-",",");
-		StringTokenizer st = new StringTokenizer(data,"-");
+		StringTokenizer st = new StringTokenizer(data, ",");
 		
 		while(st.hasMoreTokens()) {
 			CityDTO cityDTO = new CityDTO();
@@ -71,18 +44,45 @@ public class WeatherService {
 			cityDTO.setStatus(st.nextToken());
 			ar.add(cityDTO);
 		}
-
+		
+		
 		
 	}
-			
-		public void remove(ArrayList<CityDTO> ar) {
-			System.out.println("리스트 삭제");
-			String name = sc.next();
-			for(int i=0;i<ar.size();i++) {
-				if(name.equals(ar.getClass().i));
-		}
-		
-			
 	
+	public void add(ArrayList<CityDTO> ar) {
+		CityDTO cityDTO = new CityDTO();
+		System.out.println("도시명 입력");
+		cityDTO.setName(sc.next());
+		System.out.println("기온 입력");
+		cityDTO.setGion(sc.nextDouble());
+		System.out.println("습도 입력");
+		cityDTO.setHum(sc.nextInt());
+		System.out.println("날씨 입력");
+		cityDTO.setStatus(sc.next());
+		ar.add(cityDTO);
+		
 	}
+	
+	public void remove(ArrayList<CityDTO> ar) {
+		System.out.println("삭제 하려는 도시명 입력");
+		String name = sc.next();
+		
+		//전통 for
+//		for(int i=0;i<ar.size();i++) {
+//			if(name.equals(ar.get(i).getName())) {
+//				ar.remove(i);
+//				break;
+//			}
+//		}
+		
+		//향상된 for
+//		for(CityDTO cityDTO: ar) {
+//			if(name.equals(cityDTO.getName())) {
+//				ar.remove(cityDTO);
+//				break;
+//			}
+//		}
+//		
+	}
+
 }
