9ebf3bd67cda232d305043d000de1afb2a2db234
parkvsmin <sdfjkl658@naver.com>
2022-07-20 21:33:23 +0900

    11

--- src/com/iu/util/list/ex1/WeatherController.java
+++ src/com/iu/util/list/ex1/WeatherController.java
@@ -13,34 +13,10 @@ public class WeatherController {
 //	5.지역정보삭제
 //	6.종          료
 	
-	Scanner sc;
-	WeatherView wv;
-	WeatherService ws;
-	CityDTO [] CityDTOs;
-	
-	public WeatherController() {
-	
-	Scanner sc = new Scanner(System.in);
-	wv = new WeatherView();
-	ws = new WeatherService();
+	public void start() {
+		ArrayList<CityDTO> ar = new ArrayList<>();
 }
 	
-	public void start() {
-	
-		ArrayList<CityDTO> ar = new ArrayList<>();
-		boolean check=true;
-		while(check) {
-			System.out.println("1.날씨정보초기화");
-			System.out.println("2.전국날씨출력");
-			System.out.println("3.지역날씨검색");
-			System.out.println("4.지역정보추가");
-			System.out.println("5.지역정보삭제");
-			System.out.println("6.종      료");
-			int select= sc.nextInt();
-		}
-	
-		
-	}
-	
+			
 
 }
